2ee8eb8033a3777d57cfa42537a91251fd4cc320
vamsidhar1198 <84258765+vamsidhar1198@users.noreply.github.com>
2023-02-27 12:49:06 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+#pip list --this will show all the packages by pip 
+#to run this file use command streamlit run FILENAME WITH EXTENSION
+#ctrl+c to close the sever 
+import streamlit as st
+
+st.header('HELLO WORLD!')
+st.header('welcome to my page 😀')
+st.header('how are you all ?')
+#pip freeze > requirements.txt will create a requriments file in the folder 
+#put the code (app.py) and text file created in the folder in the github account 
+#for creating the enviroment python -m venv "folder name" 
+#source bin/activate to activate the enviroment
+#to close enivorment use deactivate
+#cd .. go one directory back
+#cd . stay in same directory
